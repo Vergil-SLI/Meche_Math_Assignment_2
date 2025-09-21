613edc12982d5bef1953532f19e2f4c422ab1459
Vergil-SLI <sherrylixz@gmail.com>
2025-09-20 22:06:44 -0400

    deleted stupid .asv files :D

--- projectile.m
+++ projectile.m
@@ -2,7 +2,7 @@ theta = pi/4;
 
 solver_params = struct();
 solver_params.numerical_diff = 1;
-object_dist = @(t) projectile_traj(theta,t) - target_traj(t);
+object_dist = @(t) (projectile_traj(theta,t) - target_traj(t));
 [x_guess,exit_flag] = multi_newton(object_dist, 8, solver_params);
 
 %run_simulation(theta,t_c)
